694703d26825f43b17f3bf34c724a92343c384de
Alexis Carlier <alexis.carlier0@gmail.com>
2017-09-16 14:49:27 +0100

    Step 10 corrected

--- directory.rb
+++ directory.rb
@@ -3,9 +3,7 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
   name = gets.chomp
-
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
@@ -23,12 +21,12 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit"
+  puts "9. Exit" 
 end
 
 def show_students
   print_header
-  print_students_list
+  print_student_list
   print_footer
 end
 
@@ -39,9 +37,9 @@ def process(selection)
   when "2"
     show_students
   when "9"
-    exit
+    exit # this will cause the program to terminate
   else
-    puts "I don't know what you mean, try again"
+    puts "I don't know what you meant, try again"
   end
 end
 
@@ -50,14 +48,14 @@ def print_header
   puts "-------------"
 end
 
-def print_students_list
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
