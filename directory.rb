19174f3d507a0fd094b30f40cead0d71550973b7
Alexis Carlier <alexis.carlier0@gmail.com>
2017-09-16 13:24:55 +0100

    Step 9

--- directory.rb
+++ directory.rb
@@ -1,20 +1,38 @@
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
-  puts "Please enter the names of the students and their cohorts"
+  puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
   students = []
-  name = gets.strip
-  cohort = gets.strip
-  possible_cohorts = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  name = gets.chomp
 
   while !name.empty? do
-    cohort = "No cohort" unless possible_cohorts.include?(cohort)
-    students << { name: name.to_sym, cohort: cohort.to_sym }
-    puts "Now we have 1 student" if students.count == 1
-    puts "Now we have #{students.count} students" if students.count != 1
-    name = gets.strip
-    break if name.empty?
-    cohort = gets.strip
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
   end
   students
 end
@@ -25,20 +43,16 @@ def print_header
 end
 
 def print(students)
-  if students.count > 0
-    grouped_cohorts = students.map { |student| student[:cohort] }.uniq
-    grouped_cohorts.each do |cohort|
-      students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)" if cohort == student[:cohort]
-      end
-    end
-  end 
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+interactive_menu
 students = input_students
 print_header
 print(students)
